74da3b95fc265f79a1dbfd68c4c1615f54cf1fbd
mikeumus <mike@betabulls.com>
2014-04-20 15:45:46 -0400

    Learning Centre button!

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,14 +1,14 @@
 $(function(){
 	
     function scrolledPast(){
-        
+        $('.learning-centre-btn').removeClass('opacity-zero');
     }
     function scrolledTop(){
-        
+        $('.learning-centre-btn').addClass('opacity-zero');
     }
     
 	window.onscroll = function(){
-		document.documentElement.scrollTop || document.body.scrollTop > 366 ?
+		document.documentElement.scrollTop || document.body.scrollTop > 424 ?
 			scrolledPast()
 			: scrolledTop();
 	}
