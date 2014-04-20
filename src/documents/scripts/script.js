ed5fcc103a20af107586868652d8a428b5857d0b
mikeumus <mike@betabulls.com>
2014-04-19 23:50:14 -0400

    Pushing before the clock strike 12!

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,9 +1,16 @@
 $(function(){
 	
+    function scrolledPast(){
+        
+    }
+    function scrolledTop(){
+        
+    }
+    
 	window.onscroll = function(){
 		document.documentElement.scrollTop || document.body.scrollTop > 366 ?
-			document.getElementById('main-menu').style.borderBottom = '7px solid #005EAC'
-			: document.getElementById('main-menu').style.borderBottom = 0;
+			scrolledPast()
+			: scrolledTop();
 	}
 	
 });
